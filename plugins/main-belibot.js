9f85506ed9aff674e9ba0c21deb891fbb14898b7
XzeeBotz <100888117+XzeeBotz@users.noreply.github.com>
2022-06-25 09:46:51 +0700

    Update main-belibot.js

--- plugins/main-belibot.js
+++ plugins/main-belibot.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
 const ftroli = {
     key : {
-    remoteJid: '6283136505591-1614953337@g.us',
+    remoteJid: '6281381685954-1614953337@g.us',
     participant : '0@s.whatsapp.net'
     },
     message: {
@@ -10,7 +10,7 @@ const ftroli = {
     status: 1,
     surface : 1,
     message: 'Get Bot', 
-    orderTitle: `Hyzer`,
+    orderTitle: `BotOfc`,
     thumbnail: 'https://telegra.ph/file/5ecbec3e82e247671a18e.jpg', 
     sellerJid: '0@s.whatsapp.net' 
     }
