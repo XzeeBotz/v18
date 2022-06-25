0d3d08842a0af477fd4704d0703ad0c34c4656fd
XzeeBotz <100888117+XzeeBotz@users.noreply.github.com>
2022-06-25 09:41:32 +0700

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -11,10 +11,10 @@ let ye = `@${m.sender.split`@`[0]}`
 let esce = `
 Hai ${ye} Lagi Nyari Sc Ya? 
 
-Nih Link Sc Nya Dibawah :
-https://youtu.be/AXPzjQ5jxx8
+Nih Link Sc Nya, Cari Sendiri Awokwok :
+https://github.com/XzeeBotz
 
-_*Jangan Lupa Di Subscribe*_`
+_*Jangan Lupa Follow And Star*_`
 conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
 }
 handler.help = ['sc', 'sourcecode']
