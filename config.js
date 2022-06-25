956f3708b1824228cca23e49eac04fc811f2d9dd
XzeeBotz <100888117+XzeeBotz@users.noreply.github.com>
2022-06-25 09:51:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ global.web = 'https://github.com/XzeeBotz' //ubah jadi website lu, bisa link ig,
 global.price1 = '1 bulan = 20.000\n2 Minggu 15.000\n1 Minggu 10.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
-global.fotonya1 = 'https://telegra.ph/file/e4a2f4339da8a32ad20a1.jpg' //ganti jadi foto bot mu
-global.fotonya2 = 'https://telegra.ph/file/8c471d45b6cedad43cf84.jpg' //ini juga ganti 
+global.fotonya1 = 'https://telegra.ph/file/9b704abab7252b04fa657.jpg' //ganti jadi foto bot mu
+global.fotonya2 = 'https://telegra.ph/file/9b704abab7252b04fa657.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'BOT OFFICIAL'
